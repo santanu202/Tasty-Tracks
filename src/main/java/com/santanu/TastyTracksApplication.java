361fc372a81bb701ad8918749b093e5f08a72be1
santanu1998 <santanu.singha021@gmail.com>
2024-04-16 08:05:54 +0530

    first commit

---
+++ src/main/java/com/santanu/TastyTracksApplication.java
@@ -0,0 +1,13 @@
+package com.santanu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TastyTracksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TastyTracksApplication.class, args);
+	}
+
+}
